d055c5d447a3929aa60ac27880395a65f5934d58
otouto3 <otoutoyeah@gmail.com>
2021-10-02 12:56:48 +0900

    初期プロジェクト

---
+++ CalculaotrApp/CalculaotrApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculaotrApp
+//
+//  Created by ごつ on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
